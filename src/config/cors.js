3213acf90cfbd8d3d9dadafcea65e88833b0ee7b
AlexUnju <alexxe@fi.unju.edu.ar>
2025-07-03 15:40:55 -0300

    refactor: clubs

--- src/config/cors.js
+++ src/config/cors.js
@@ -5,7 +5,7 @@
 const corsOptions = {
     origin: process.env.CORS_ORIGIN || 'http://localhost:4200',
     credentials: true,
-    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+    methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
     preflightContinue: false,
     optionsSuccessStatus: 204
